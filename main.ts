9ffe34e78b454c91e0cb6ad1c68ee571dea53412
CapDDee <107929648+CapDDee@users.noreply.github.com>
2022-06-28 15:23:47 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,62 +6,31 @@ namespace SpriteKind {
     export const Fenemy = SpriteKind.create()
     export const Benemy = SpriteKind.create()
     export const plus1 = SpriteKind.create()
+    export const icon = SpriteKind.create()
 }
 namespace StatusBarKind {
     export const bosshealth = StatusBarKind.create()
     export const BigEnemyHealth = StatusBarKind.create()
 }
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Fenemy, function (sprite3, otherSprite3) {
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Fenemy, function (sprite, otherSprite) {
     music.bigCrash.play()
     info.changeLifeBy(-1)
-    enemydeath(otherSprite3)
+    enemydeath(otherSprite)
     scene.cameraShake(4, 500)
 })
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Benemy, function (sprite22, otherSprite22) {
-    sprite22.destroy()
-    statusbars.getStatusBarAttachedTo(StatusBarKind.BigEnemyHealth, otherSprite22).value += -5
+sprites.onOverlap(SpriteKind.Player, SpriteKind.plus1, function (sprite, otherSprite) {
+    music.magicWand.play()
+    info.changeLifeBy(1)
+    otherSprite.destroy()
 })
 sprites.onOverlap(SpriteKind.sheild, SpriteKind.Enemy, function (sprite, otherSprite) {
     enemydeath(otherSprite)
     info.changeScoreBy(1)
     myShield.destroy()
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.PowerUP, function (sprite42, otherSprite42) {
-    music.beamUp.play()
-    doubleshoot = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . 8 . . . . . . . . . . . 
-        . . . . 8 8 . 8 4 . . . . . . . 
-        . . . . 4 8 8 a a 2 . . . . . . 
-        . . . . 8 8 . 8 4 . . . . . . . 
-        . . . . 8 . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . 8 . . . . . . . . . . . 
-        . . . . 8 8 . 8 4 . . . . . . . 
-        . . . . . 8 8 a a 2 . . . . . . 
-        . . . . 8 8 . 8 4 . . . . . . . 
-        . . . . 8 . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, SpriteKind.Player)
-    doubleshoot.setPosition(39, 7)
-    doubleshoot.lifespan = 5000
-    otherSprite42.destroy()
-})
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite7, otherSprite7) {
-    sprite7.destroy()
-    statusbars.getStatusBarAttachedTo(StatusBarKind.EnemyHealth, otherSprite7).value += -20
-})
-sprites.onOverlap(SpriteKind.Player, SpriteKind.plus1, function (sprite4, otherSprite4) {
-    music.magicWand.play()
-    info.changeLifeBy(1)
-    otherSprite4.destroy()
-})
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Fenemy, function (sprite2, otherSprite2) {
-    sprite2.destroy()
-    statusbars.getStatusBarAttachedTo(StatusBarKind.EnemyHealth, otherSprite2).value += -20
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Fenemy, function (sprite, otherSprite) {
+    sprite.destroy()
+    statusbars.getStatusBarAttachedTo(StatusBarKind.EnemyHealth, otherSprite).value += -20
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     projectile = sprites.createProjectileFromSprite(img`
@@ -111,18 +80,6 @@ statusbars.onZero(StatusBarKind.EnemyHealth, function (status) {
     info.changeScoreBy(1)
     enemydeath(status.spriteAttachedTo())
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite8, otherSprite8) {
-    music.bigCrash.play()
-    info.changeLifeBy(-1)
-    enemydeath(otherSprite8)
-    scene.cameraShake(4, 500)
-})
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Benemy, function (sprite32, otherSprite32) {
-    music.bigCrash.play()
-    info.changeLifeBy(-2)
-    enemydeath(otherSprite32)
-    scene.cameraShake(4, 500)
-})
 function spawnFEnemy () {
     fastenemy = sprites.create(img`
         . . . . . . . . . . . . . . . . 
@@ -146,6 +103,7 @@ function spawnFEnemy () {
     fastenemy.vx = 0 - fastenemyspeed
     fastenemy.y = randint(20, scene.screenHeight() - 40)
     statusbar = statusbars.create(25, 3, StatusBarKind.EnemyHealth)
+    statusbar.setColor(0, 0)
     statusbar.attachToSprite(fastenemy)
     pause(FenemySpawn)
     fastenemy.setFlag(SpriteFlag.DestroyOnWall, true)
@@ -187,55 +145,68 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     })
 })
 function spawnBenemy () {
-    BEnemy = sprites.create(img`
-        ................................
-        ................................
-        ................................
-        ..........22222222222222........
-        ..........22222222222222........
-        .........22f222222222f2222......
-        .........222f2222222f22222......
-        .......222222ff222ff2222222.....
-        ......222252222222225222222.....
-        ......222255222222255222222.....
-        .....22222555222225552222222....
-        .....222225552222255522222222...
-        ....222225ff5222225ff52222222...
-        ....222225ff5222225ff52222222...
-        .....222222222222222222222222...
-        .....22222222e22e222222222222...
-        .....22222222222222222222222....
-        .......2222222222222222222......
-        .......2222222222222222222......
-        .......222ee4eeeee44e22222......
-        .......2222eeeeeeeee232222......
-        .......22232222222222322222.....
-        .......22.32..2..2...32...2.....
-        .......52.32..2..2...32...2.....
-        .......52.32..2..2...32...25....
-        .......22.32..2..2...32...25....
-        .......2..32.22..22..32...22....
-        ......22..22.25..52....2...2....
-        .....22..22..25..552...22..2....
-        .....2..23...22...52...32..2....
-        .....2..23....2...22...32..2....
-        .....2..23....2...2....32..2....
-        `, SpriteKind.Benemy)
-    BEnemy.x = scene.screenWidth()
-    BEnemy.vx = 0 - Benemyspeed
-    BEnemy.y = randint(20, scene.screenHeight() - 40)
-    statusBig = statusbars.create(40, 3, StatusBarKind.BigEnemyHealth)
-    statusBig.attachToSprite(BEnemy)
-    pause(BEnemySpawn)
-    BEnemy.setFlag(SpriteFlag.DestroyOnWall, true)
+    timer.throttle("action", randint(2000, 6000), function () {
+        BEnemy = sprites.create(img`
+            ................................
+            ................................
+            ................................
+            ..........22222222222222........
+            ..........22222222222222........
+            .........22f222222222f2222......
+            .........222f2222222f22222......
+            .......222222ff222ff2222222.....
+            ......222252222222225222222.....
+            ......222255222222255222222.....
+            .....22222555222225552222222....
+            .....222225552222255522222222...
+            ....222225ff5222225ff52222222...
+            ....222225ff5222225ff52222222...
+            .....222222222222222222222222...
+            .....22222222e22e222222222222...
+            .....22222222222222222222222....
+            .......2222222222222222222......
+            .......2222222222222222222......
+            .......222ee4eeeee44e22222......
+            .......2222eeeeeeeee232222......
+            .......22232222222222322222.....
+            .......22.32..2..2...32...2.....
+            .......52.32..2..2...32...2.....
+            .......52.32..2..2...32...25....
+            .......22.32..2..2...32...25....
+            .......2..32.22..22..32...22....
+            ......22..22.25..52....2...2....
+            .....22..22..25..552...22..2....
+            .....2..23...22...52...32..2....
+            .....2..23....2...22...32..2....
+            .....2..23....2...2....32..2....
+            `, SpriteKind.Benemy)
+        BEnemy.x = scene.screenWidth()
+        BEnemy.vx = 0 - Benemyspeed
+        BEnemy.y = randint(20, scene.screenHeight() - 40)
+        statusBig = statusbars.create(40, 3, StatusBarKind.BigEnemyHealth)
+        statusbar.setColor(0, 0)
+        statusBig.attachToSprite(BEnemy)
+        pause(BEnemySpawn)
+        BEnemy.setFlag(SpriteFlag.DestroyOnWall, true)
+    })
 }
 statusbars.onZero(StatusBarKind.BigEnemyHealth, function (status2) {
     info.changeScoreBy(2)
     enemydeath(status2.spriteAttachedTo())
 })
-sprites.onOverlap(SpriteKind.sheild, SpriteKind.Benemy, function (sprite6, otherSprite6) {
-    enemydeath(otherSprite6)
-    info.changeScoreBy(2)
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Benemy, function (sprite, otherSprite) {
+    music.bigCrash.play()
+    info.changeLifeBy(-2)
+    enemydeath(otherSprite)
+    scene.cameraShake(4, 500)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Benemy, function (sprite, otherSprite) {
+    sprite.destroy()
+    statusbars.getStatusBarAttachedTo(StatusBarKind.BigEnemyHealth, otherSprite).value += -5
+})
+sprites.onOverlap(SpriteKind.sheild, SpriteKind.Fenemy, function (sprite, otherSprite) {
+    enemydeath(otherSprite)
+    info.changeScoreBy(1)
     myShield.destroy()
 })
 function spawnEnemy () {
@@ -261,10 +232,44 @@ function spawnEnemy () {
     enemyalien.vx = 0 - enemyspeed
     enemyalien.y = randint(20, scene.screenHeight() - 40)
     statusbar = statusbars.create(25, 3, StatusBarKind.EnemyHealth)
+    statusbar.setColor(0, 0)
     statusbar.attachToSprite(enemyalien)
     pause(enemySpawn)
     enemyalien.setFlag(SpriteFlag.DestroyOnWall, true)
 }
+sprites.onOverlap(SpriteKind.Player, SpriteKind.PowerUP, function (sprite, otherSprite) {
+    music.beamUp.play()
+    doubleshoot = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . 8 . . . . . . . . . . . 
+        . . . . 8 8 . 8 4 . . . . . . . 
+        . . . . 4 8 8 a a 2 . . . . . . 
+        . . . . 8 8 . 8 4 . . . . . . . 
+        . . . . 8 . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . 8 . . . . . . . . . . . 
+        . . . . 8 8 . 8 4 . . . . . . . 
+        . . . . . 8 8 a a 2 . . . . . . 
+        . . . . 8 8 . 8 4 . . . . . . . 
+        . . . . 8 . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.icon)
+    doubleshoot.setPosition(39, 7)
+    doubleshoot.lifespan = 5000
+    otherSprite.destroy()
+})
+sprites.onOverlap(SpriteKind.sheild, SpriteKind.Benemy, function (sprite, otherSprite) {
+    enemydeath(otherSprite)
+    info.changeScoreBy(2)
+    myShield.destroy()
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprite.destroy()
+    statusbars.getStatusBarAttachedTo(StatusBarKind.EnemyHealth, otherSprite).value += -20
+})
 function enemydeath (enemy: Sprite) {
     enemy.destroy(effects.spray, 500)
     if (Math.percentChance(20)) {
@@ -314,10 +319,11 @@ function enemydeath (enemy: Sprite) {
         plus12.lifespan = 4000
     }
 }
-sprites.onOverlap(SpriteKind.sheild, SpriteKind.Fenemy, function (sprite5, otherSprite5) {
-    enemydeath(otherSprite5)
-    info.changeScoreBy(1)
-    myShield.destroy()
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    music.bigCrash.play()
+    info.changeLifeBy(-1)
+    enemydeath(otherSprite)
+    scene.cameraShake(4, 500)
 })
 let plus12: Sprite = null
 let powerup: Sprite = null
@@ -326,8 +332,8 @@ let statusBig: StatusBarSprite = null
 let BEnemy: Sprite = null
 let statusbar: StatusBarSprite = null
 let fastenemy: Sprite = null
-let projectile: Sprite = null
 let doubleshoot: Sprite = null
+let projectile: Sprite = null
 let myShield: Sprite = null
 let Benemyspeed = 0
 let BEnemySpawn = 0
@@ -358,6 +364,7 @@ Ship = sprites.create(img`
 Ship.setPosition(8, 55)
 controller.moveSprite(Ship)
 Ship.setStayInScreen(true)
+let vie = info.life()
 info.setLife(5)
 enemyspeed = 20
 enemySpawn = 2000
